3dc351210ab5ae62a24e87141879215b511c53ab
Serge Modin <modinsv@yandex.ru>
2020-02-08 10:30:14 +0300

    Added Image with this.props in the UserItem component

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "id",
+      login: "jetleebruce",
+      avatar_url: "https://avatars3.githubusercontent.com/u/12411736?s=460&v=4",
+      html_url: "https://github.com/jetleebruce"
+    };
+  }
   render() {
-    return <div>UserItem</div>;
+    return (
+      <div className='card text-center'>
+        <img
+          src={this.state.avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: "60px" }}
+        />
+      </div>
+    );
   }
 }
 
