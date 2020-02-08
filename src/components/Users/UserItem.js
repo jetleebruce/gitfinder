2457da62e458d09b67596622851a46141f376f89
Serge Modin <modinsv@yandex.ru>
2020-02-08 10:39:26 +0300

    Refactoring state

--- src/components/Users/UserItem.js
+++ src/components/Users/UserItem.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
+ state = {
       id: "id",
       login: "jetleebruce",
       avatar_url: "https://avatars3.githubusercontent.com/u/12411736?s=460&v=4",
       html_url: "https://github.com/jetleebruce"
     };
-  }
+  
   render() {
+
+    const {login, avatar_url, html_url} = this.state;
     return (
       <div className='card text-center'>
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt=''
           className='round-img'
           style={{ width: "60px" }}
         />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
       </div>
     );
   }
